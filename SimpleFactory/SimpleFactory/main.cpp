d755cfe2c513980b0b4a06896d3a4d8ef080cd08
yunGit <yun@YundeiMac.local>
2014-08-27 14:56:58 +0800

     perfect the simpleFactor

--- SimpleFactory/SimpleFactory/main.cpp
+++ SimpleFactory/SimpleFactory/main.cpp
@@ -7,12 +7,19 @@
 //
 
 #include <iostream>
+#include "Factory.h"
+#include "Operator.h"
 
 int main(int argc, const char * argv[])
 {
-
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Factory* factory = new Factory();
+    Operator* whatOperator = factory -> getOperator("+", 4, 5);
+    float result = whatOperator -> getResault();
+    std::cout<<"result is"<<result<<std::endl;
+
     return 0;
 }
 
